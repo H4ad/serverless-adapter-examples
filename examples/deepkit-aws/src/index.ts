077119e9ef05a4934910e54c75821f49ec06a8d0
Vinícius Lourenço <vinyygamerlol@gmail.com>
2022-11-27 16:56:15 -0300

    feat(cors): added cors framework

--- examples/deepkit-aws/src/index.ts
+++ examples/deepkit-aws/src/index.ts
@@ -5,14 +5,17 @@ import { HttpDeepkitFramework } from '@h4ad/serverless-adapter/lib/frameworks/de
 import { DefaultHandler } from '@h4ad/serverless-adapter/lib/handlers/default';
 import { PromiseResolver } from '@h4ad/serverless-adapter/lib/resolvers/promise';
 import { createApp } from './setup';
+import { CorsFramework } from '@h4ad/serverless-adapter/lib/frameworks/cors';
 
 const app = createApp();
 const httpKernel = app.get(HttpKernel);
+const framework = new HttpDeepkitFramework();
+const corsFramework = new CorsFramework(framework);
 
 export const handler = ServerlessAdapter.new(httpKernel)
   .setHandler(new DefaultHandler())
   .setResolver(new PromiseResolver())
-  .setFramework(new HttpDeepkitFramework())
+  .setFramework(corsFramework)
   .setLogger(createDefaultLogger())
   .addAdapter(new ApiGatewayV2Adapter())
   .build();
--- examples/trpc-aws/src/index.ts
+++ examples/trpc-aws/src/index.ts
@@ -4,11 +4,13 @@ import { TrpcFramework } from '@h4ad/serverless-adapter/lib/frameworks/trpc';
 import { DefaultHandler } from '@h4ad/serverless-adapter/lib/handlers/default';
 import { PromiseResolver } from '@h4ad/serverless-adapter/lib/resolvers/promise';
 import { appRouter, frameworkOptions, TrpcContext } from './setup';
+import { CorsFramework } from '@h4ad/serverless-adapter/lib/frameworks/cors';
 
 const framework = new TrpcFramework<TrpcContext>(frameworkOptions);
+const corsFramework = new CorsFramework(framework);
 
 export const handler = ServerlessAdapter.new(appRouter)
-  .setFramework(framework)
+  .setFramework(corsFramework)
   .setHandler(new DefaultHandler())
   .setResolver(new PromiseResolver())
   .addAdapter(new ApiGatewayV2Adapter())
