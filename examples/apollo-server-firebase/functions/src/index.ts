1a43e2ed866d5635fcf7ffc079bf85213eff5ebf
Vinícius Lourenço <vinyygamerlol@gmail.com>
2022-12-13 21:56:54 -0300

    fix(firebase): issues with firebase deployment

--- examples/apollo-server-firebase/functions/src/index.ts
+++ examples/apollo-server-firebase/functions/src/index.ts
@@ -1,24 +1,20 @@
 import { ServerlessAdapter } from '@h4ad/serverless-adapter';
 import { DummyAdapter } from '@h4ad/serverless-adapter/lib/adapters/dummy';
 import { ApolloServerFramework, DefaultServerlessApolloServerContext } from '@h4ad/serverless-adapter/lib/frameworks/apollo-server';
+import { JsonBodyParserFramework } from '@h4ad/serverless-adapter/lib/frameworks/body-parser';
 import { CorsFramework } from '@h4ad/serverless-adapter/lib/frameworks/cors';
 import { HttpFirebaseHandler } from '@h4ad/serverless-adapter/lib/handlers/firebase';
 import { PromiseResolver } from '@h4ad/serverless-adapter/lib/resolvers/promise';
 import { app } from './setup';
 
 const framework = new ApolloServerFramework<DefaultServerlessApolloServerContext>();
-
-// optional: you can configure cors using this guy here, if you don't want, just erase
-const corsFramework = new CorsFramework(framework, {
-  origin: '*',
-  maxAge: 30,
-});
+const jsonFramework = new JsonBodyParserFramework(framework)
 
 // needed to start the application, without this, the apollo server will throw an error
 app.startInBackgroundHandlingStartupErrorsByLoggingAndFailingAllRequests();
 
 export const main = ServerlessAdapter.new(app)
-  .setFramework(corsFramework)
+  .setFramework(jsonFramework)
   .setHandler(new HttpFirebaseHandler())
   .setResolver(new PromiseResolver())
   .addAdapter(new DummyAdapter())
--- examples/deepkit-firebase/functions/src/index.ts
+++ examples/deepkit-firebase/functions/src/index.ts
@@ -1,17 +1,22 @@
 import { HttpKernel } from '@deepkit/http';
 import { ServerlessAdapter } from '@h4ad/serverless-adapter';
 import { DummyAdapter } from '@h4ad/serverless-adapter/lib/adapters/dummy';
+import { JsonBodyParserFramework } from '@h4ad/serverless-adapter/lib/frameworks/body-parser';
 import { HttpDeepkitFramework } from '@h4ad/serverless-adapter/lib/frameworks/deepkit';
 import { HttpFirebaseHandler } from '@h4ad/serverless-adapter/lib/handlers/firebase';
 import { DummyResolver } from '@h4ad/serverless-adapter/lib/resolvers/dummy';
 import { createApp } from './setup';
 
-const app = createApp()
+const app = createApp();
 const httpKernel = app.get(HttpKernel);
+
+const jsonFramework = new JsonBodyParserFramework(
+  new HttpDeepkitFramework(),
+);
 
 const handler = ServerlessAdapter.new(httpKernel)
   .setHandler(new HttpFirebaseHandler())
-  .setFramework(new HttpDeepkitFramework())
+  .setFramework(jsonFramework)
   .setResolver(new DummyResolver())
   .addAdapter(new DummyAdapter())
   .build();
